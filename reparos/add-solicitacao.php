d06ce7e821f8ad3b75022c56e876c3d0fedb0908
andre santos <andrekrt1922@gmail.com>
2021-07-14 08:11:16 -0300

    correçao no add Solicitaçao

--- reparos/add-solicitacao.php
+++ reparos/add-solicitacao.php
@@ -9,22 +9,33 @@ if(isset($_SESSION['idUsuario']) && empty($_SESSION['idUsuario']) == false ){
     $peca = ucfirst(filter_input(INPUT_POST, 'peca')) ;
     $placa = strtoupper(filter_input(INPUT_POST, 'veiculo'));
     $descricao = ucfirst(filter_input(INPUT_POST,'descricao'));
-    $imagem = $_FILES['imagem']['name'];
+    $imagem = $_FILES['imagem']['name']?$_FILES['imagem']['name']:null;
     $situacao = "Em análise";
     $idSolic = $_SESSION['idUsuario'];
     $localReparo = ucfirst(filter_input(INPUT_POST, 'local')) ;
     $categoria = filter_input(INPUT_POST, 'categoria');
 
-    
-    $inserindo = "INSERT INTO solicitacoes(dataAtual, servico, descricao, imagem, placarVeiculo, idSolic, statusSolic, localReparo, categoria_idcategoria) VALUES ('$dataAtual','$peca', '$descricao', '$imagem', '$placa', '$idSolic', '$situacao', '$localReparo','$categoria')";
+    echo "$dataAtual<br>$peca<br>$placa<br>$descricao<br>$imagem<br> $situacao<br>$idSolic<br>$localReparo<br>$categoria<br>";
 
-    $sql = $db->query($inserindo);
+    
+    $inserindo = "";
+
+    $sql = $db->prepare("INSERT INTO solicitacoes(dataAtual, servico, descricao, imagem, placarVeiculo, idSolic, statusSolic, localReparo, categoria_idcategoria) VALUES (:datAtual,:peca, :descricao, :imagem, :placa, :idSolic, :situacao, :localReparo,:categoria)");
+    $sql->bindValue(':datAtual', $dataAtual);
+    $sql->bindValue(':peca', $peca);
+    $sql->bindValue(':descricao', $descricao);
+    $sql->bindValue(':imagem', $imagem);
+    $sql->bindValue(':placa', $placa);
+    $sql->bindValue(':idSolic', $idSolic);
+    $sql->bindValue(':situacao', $situacao);
+    $sql->bindValue(':localReparo', $localReparo);
+    $sql->bindValue(':categoria', $categoria);
     $pasta = 'uploads/';
     $mover = move_uploaded_file($_FILES['imagem']['tmp_name'],$pasta.$imagem);
-    if($sql){
+    if($sql->execute()){
         header("Location:solicitacoes.php");
     }else{
-        echo "Não foi cadastrado";
+        print_r($sql->errorInfo());
     }
 
 }
