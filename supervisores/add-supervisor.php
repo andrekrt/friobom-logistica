d28662edeeedd1fef48e3b20a62d432ffeaeaa0f
andrekrt <andrekrt1922@gmail.com>
2023-04-11 11:04:10 -0300

    verificacao de supervisor

--- supervisores/add-supervisor.php
+++ supervisores/add-supervisor.php
@@ -9,18 +9,29 @@ if(isset($_SESSION['idUsuario']) && empty($_SESSION['idUsuario'])==false && ($_S
     $nome = filter_input(INPUT_POST, 'nome');
     $cidade = filter_input(INPUT_POST, 'residencia');
     $veiculo = filter_input(INPUT_POST, 'veiculo');
-    
-    $sql = $db->prepare("INSERT INTO supervisores (idsupervisor, nome_supervisor, cidade_residencia, veiculo) VALUES (:codigo, :supervisor, :cidade, :veiculo)");
-    $sql->bindValue(':codigo', $codigo);
-    $sql->bindValue(':supervisor', $nome);
-    $sql->bindValue(':cidade', $cidade);
-    $sql->bindValue(':veiculo', $veiculo);
-    if($sql->execute()){
-        echo "<script> alert('Supervisor Cadastrado!')</script>";
+
+    //verificar se já existe cadastro
+    $sqlConsulta = $db->prepare("SELECT idsupervisor FROM supervisores WHERE idsupervisor=:codigo");
+    $sqlConsulta->bindValue(':codigo', $codigo);
+    $sqlConsulta->execute();
+    if($sqlConsulta->rowCount()>0){
+        echo "<script> alert('Esse supervisor já está cadastrado!')</script>";
         echo "<script> window.location.href='supervisores.php' </script>";
     }else{
-        print_r($sql->errorInfo());
-    }    
+        $sql = $db->prepare("INSERT INTO supervisores (idsupervisor, nome_supervisor, cidade_residencia, veiculo) VALUES (:codigo, :supervisor, :cidade, :veiculo)");
+        $sql->bindValue(':codigo', $codigo);
+        $sql->bindValue(':supervisor', $nome);
+        $sql->bindValue(':cidade', $cidade);
+        $sql->bindValue(':veiculo', $veiculo);
+        if($sql->execute()){
+            echo "<script> alert('Supervisor Cadastrado!')</script>";
+            echo "<script> window.location.href='supervisores.php' </script>";
+        }else{
+            print_r($sql->errorInfo());
+        }    
+    }
+    
+    
 
 }else{
     echo "<script> alert('Acesso não permitido!!!')</script>";
