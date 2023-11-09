8ec7d388dba4c822cfad8d3bf962178252c2b8be
andrekrt <andrekrt1922@gmail.com>
2023-11-09 14:41:16 -0300

    relatorio extrato

--- pneus/extrato-xls.php
+++ pneus/extrato-xls.php
@@ -3,7 +3,7 @@
 session_start();
 require("../conexao.php");
 
-$idModudulo = 11;
+$idModudulo = 12;
 $idUsuario = $_SESSION['idUsuario'];
 
 $sqlPerm = $db->prepare("SELECT COUNT(*) FROM permissoes WHERE idusuario=:usuario AND idmodulo=:modulo");
@@ -15,27 +15,30 @@ $result = $sqlPerm->fetchColumn();
 if (isset($_SESSION['idUsuario']) && empty($_SESSION['idUsuario']) == false && ($result>0)  ) {
 
     $db->exec("set names utf8");
-    $sql = $db->query("SELECT idservicos, descricao, categoria, nome_usuario FROM servicos_almoxarifado LEFT JOIN usuarios ON servicos_almoxarifado.usuario = usuarios.idusuarios");
+    $sql = $db->query("SELECT * FROM  extrato_pneu LEFT JOIN pneus ON extrato_pneu.pneu = pneus.idpneus");
+    $dados = $sql->fetchAll();
 
-    header('Content-Type:text/csv; charset=UTF-8');
-    header('Content-Disposition: attachement; filename=extrato.csv');
+    $fp = fopen("extrato.csv", "w");
+    $escreve = fwrite($fp, "ID;". mb_convert_encoding('Nº Fogo', 'ISO-8859-1', 'UTF-8') .";". mb_convert_encoding('Data Operação','ISO-8859-1', 'UTF-8') .";". mb_convert_encoding('Operação', 'ISO-8859-1', 'UTF-8') ."; Km Pneu;".mb_convert_encoding('Veículo','ISO-8859-1', 'UTF-8') ." ;".mb_convert_encoding('Km Veículo','ISO-8859-1', 'UTF-8'));
 
-    $arquivo = fopen("php://output", "w");
-
-    $cabacelho = [
-        "ID",
-        "Pneu"
-    ];
-    
-    fputcsv($arquivo, $cabacelho, ';');
-
-    $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
     foreach($dados as $dado){
-        
-        fputcsv($arquivo, mb_convert_encoding($dado, 'ISO-8859-1', 'UTF-8') , ';');
+        $escreve=fwrite($fp,
+            "\n".$dado['idextrato'].";" . $dado['num_fogo'] .";". date("d/m/Y",strtotime($dado['data_op'])).";". mb_convert_encoding($dado['operacao'],'ISO-8859-1', 'UTF-8') .";".  $dado['km_pneu'] ."; $dado[veiculo]; $dado[km_veiculo];". ""
+        );
     }
 
-    fclose($arquivo);
+    fclose($fp);
+
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-Disposition: attachment; filename=extrato.csv");
+    header("Content-Type: application/zip");
+    header("Content-Transfer-Encoding: binary");
+
+    // Read the file
+    readfile('extrato.csv');
+    exit;
+
 }
 
 
